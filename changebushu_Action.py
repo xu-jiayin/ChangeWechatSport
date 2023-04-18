312a0cff3b5e6b614e7930f07dce64adaee10993
xu-jiayin <89209791+xu-jiayin@users.noreply.github.com>
2023-04-18 23:24:14 +0800

    Update changebushu_Action.py

--- changebushu_Action.py
+++ changebushu_Action.py
@@ -107,6 +107,6 @@ if __name__ == "__main__":
     user = os.environ['USER_PHONE']
     password = os.environ['USER_PWD']
     step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
-    # step = os.environ['STEP']
-    step = str(randint(26000, 30000)) 
+    step = os.environ['STEP']
+#     step = str(randint(26000, 30000)) 
     main()
